1cb88ce3febeca851bc0a1bd3c0af34bb9e6ed58
Denizyo <71915057+Denizyo@users.noreply.github.com>
2021-06-05 14:39:01 -0700

    Add files via upload

---
+++ iterateDRI.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export PATH=~/autodock/autodock_vina_1_1_2_linux_x86/bin:$PATH
+
+nameOfTheCompound=$1
+numberOfIterations=$2
+
+END=100
+
+
+rm -rf $nameOfTheCompound
+mkdir -p $nameOfTheCompound
+cp -af conf_$nameOfTheCompound.txt $nameOfTheCompound/conf.txt
+cp -af spikeprotein.pdb $nameOfTheCompound
+cp -af spikeprotein.pdbqt $nameOfTheCompound
+cp -af $nameOfTheCompound.pdbqt $nameOfTheCompound
+cd $nameOfTheCompound
+
+
+touch outputAll
+for (( i=1; i<=$END; i++ ))
+do
+   echo "Iteration: $i / $NumberOfIterations"
+   vina --config conf.txt --log logSO_$i.txt
+   mv vina_outSO.pdbqt out$i.pdbqt
+   cat logSO_$i.txt >> outputAll
+done
+
